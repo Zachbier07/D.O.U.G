7ced15174e40a52c569069a8b516233a209c699c
Zachbier07 <zachbier79@gmail.com>
2025-11-08 20:31:34 -0500

    Update detector_talker.py

--- my_detector_pkg/my_detector_pkg/detector_talker.py
+++ my_detector_pkg/my_detector_pkg/detector_talker.py
@@ -5,15 +5,33 @@ from my_detector_pkg.handTracking import HandTracker
 from std_msgs.msg import Bool
 
 
-class JazzyTalker(Node):
+class Talker(Node):
+
     def __init__(self):
         super().__init__('Talker')
         self.tracker = HandTracker() 
         self.publisher = self.create_publisher(Bool, 'handDetection', 10)
+        self.sub = self.create_subscription(
+            Bool,
+            'disarmedEnabled',
+            self.toggle_callback,
+            10
+        )
+        self.disarmed = False
+    
+    def toggle_callback(self, msg):
+        self.disarmed = msg.data
 
     def run(self):
         while rclpy.ok():
-            detected = self.tracker.process_frame()
+            # Allow callbacks (subscriber) to run
+            rclpy.spin_once(self, timeout_sec=0)
+
+            if self.disarmed == False:
+                detected = self.tracker.process_frame()
+            else:
+                detected = False
+                self.get_logger().info("Disarmed")
 
             self.get_logger().info(str(detected))
 
@@ -25,7 +43,7 @@ class JazzyTalker(Node):
 
 def main(args=None):
     rclpy.init(args=args)
-    node = JazzyTalker()
+    node = Talker()
     try:
         node.run()
     except KeyboardInterrupt:
