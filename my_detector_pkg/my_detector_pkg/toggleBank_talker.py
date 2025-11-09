9df97979bc819b32189a0032c4697839e55f3b62
RemigioGrosso <rreg2006@gmail.com>
2025-11-08 21:16:12 -0500

    button for jetson

--- my_detector_pkg/my_detector_pkg/toggleBank_talker.py
+++ my_detector_pkg/my_detector_pkg/toggleBank_talker.py
@@ -3,6 +3,41 @@ from my_detector_pkg.toggleBank import Button
 from rclpy.node import Node
 import time
 from std_msgs.msg import Bool
+import Jetson.GPIO as GPIO
+
+
+import Jetson.GPIO as GPIO
+import time
+
+class Button:
+    def __init__(self):
+        # Set up the GPIO pin you wired your button to (physical pin 18 = BCM 24)
+        self.pin = 13
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+        # Internal state to avoid multiple triggers from one press
+        self.last_state = GPIO.input(self.pin)
+        self.last_time = time.time()
+
+    def getButton(self, debounce_time=0.2):
+        current_state = GPIO.input(self.pin)
+
+        # Detect press 
+        if self.last_state == GPIO.HIGH and current_state == GPIO.LOW:
+            now = time.time()
+            if now - self.last_time > debounce_time:
+                self.last_time = now
+                self.last_state = current_state
+                return True
+
+        # Update state
+        self.last_state = current_state
+        return False
+
+    def cleanup(self):
+        GPIO.cleanup(self.pin)
+
 
 
 class Talker(Node):
